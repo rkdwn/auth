f35f3a35b1336a010b87d941b641860cd72659e5
cozy-ho <kj2693119@gmail.com>
2023-05-13 15:11:08 +0900

    [Dev] add todo

--- src/modules/interaction/interaction.module.ts
+++ src/modules/interaction/interaction.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { InteractionService } from "./interaction.service";
 import { InteractionController } from "./interaction.controller";
 
+// TODO: inject oidc module here
 @Module({
   imports: [],
   providers: [InteractionService],
